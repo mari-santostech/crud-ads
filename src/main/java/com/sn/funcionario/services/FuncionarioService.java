b1bd75e7677dafef1e44f062bf97a39bff9b0cb9
unknown <mariandeoliveirasanntos@gmail.com>
2026-08-04 22:24:39 -0300

    service meio pronto

---
+++ src/main/java/com/sn/funcionario/services/FuncionarioService.java
@@ -0,0 +1,29 @@
+package com.sn.funcionario.services;
+
+import com.sn.funcionario.models.FuncionarioModel;
+import com.sn.funcionario.repositories.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class  FuncionarioService {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    public List<FuncionarioModel> findAll(){
+       return funcionarioRepository.findAll();
+    }
+
+    public FuncionarioModel buscaporId(Long id){
+        return funcionarioRepository.findById(id).get();
+    }
+
+    public FuncionarioModel criarPessoa(FuncionarioModel funcionarioModel){
+        return funcionarioRepository.save(funcionarioModel);
+    }
+
+
+}
